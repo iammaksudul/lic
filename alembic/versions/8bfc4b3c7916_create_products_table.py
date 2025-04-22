36822d9e8d0c69283c3de7e09eb1c4058cc53d39
Kh Maksudul Alam <56459189+iammaksudul@users.noreply.github.com>
2025-04-22 07:45:49 +0600

    Add files via upload

---
+++ alembic/versions/8bfc4b3c7916_create_products_table.py
@@ -0,0 +1,24 @@
+"""Create products table
+
+Revision ID: 8bfc4b3c7916
+Revises: 
+Create Date: 2025-04-22 06:38:42.048148
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8bfc4b3c7916'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass 
